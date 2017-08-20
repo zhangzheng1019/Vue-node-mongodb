c70ad5a19366dd3856ee535cbb4520a289683aad
zhangzheng1019 <447590461@qq.com>
2017-08-20 23:57:37 +0800

    初始化项目

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<botton @click="
+		increment">+</botton>
+		<botton v-on:click="decrement">-</botton>
+		<p><span>{{ num  }}</span></p>
+	</div>
+</template>
+<style type="text/css"></style>
+<script>
+	
+	export default {
+		name: 'hello',
+		props: ['num'],
+		data() {
+			return {
+				num: 0
+			}
+		},
+
+		methods: {
+			increment() {
+				this.$emit('incre');
+			},
+			decrement() {
+				this.$emit('descre');
+			}
+		}
+	}
+</script>
